717126ecddf7fa6ac206f24a7525c5db92718e25
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2020-03-06 14:18:50 -0300

    test: force coverage to be at 100%

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/build/',
+    'lib/test-utils.test.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   moduleFileExtensions: ['ts', 'js'],
   modulePaths: ['<rootDir>/lib/'],
   preset: 'ts-jest',
