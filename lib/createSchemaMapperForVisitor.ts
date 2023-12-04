7c81a115ea63daff4ab683e2f5d1efd1fa190bf2
Felipe Bergamin <felipebergamin@profusion.mobi>
2023-12-04 11:44:53 -0300

    fix: mapping directives to object fields

--- lib/createSchemaMapperForVisitor.ts
+++ lib/createSchemaMapperForVisitor.ts
@@ -2,7 +2,6 @@ import type { SchemaMapper } from '@graphql-tools/utils';
 import { getDirective, MapperKind, mapSchema } from '@graphql-tools/utils';
 import type {
   DirectiveLocation,
-  GraphQLArgument,
   GraphQLFieldConfig,
   GraphQLObjectType,
   GraphQLSchema,
@@ -17,53 +16,43 @@ export const createMapper = <T extends DirectiveLocation>(
   directiveName: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   visitor: EasyDirectiveVisitor<any, any, T>,
-): SchemaMapper => ({
-  [MapperKind.QUERY](query, schema): GraphQLObjectType {
-    visitor.visitQuery(query, schema, directiveName);
-    return query;
-  },
-  [MapperKind.MUTATION](mutation, schema): GraphQLObjectType {
-    visitor.visitMutation(mutation, schema, directiveName);
-    return mutation;
-  },
-  [MapperKind.OBJECT_TYPE](type, schema): GraphQLObjectType {
-    Object.values(type.getFields()).forEach(field => {
-      field.args.forEach(arg => {
-        const [directive] = getDirective(schema, arg, directiveName) ?? [];
-        if (!directive) return;
-        // eslint-disable-next-line no-param-reassign
-        visitor.args = directive;
-        visitor.visitArgumentDefinition(arg as GraphQLArgument, {
-          field,
+): SchemaMapper => {
+  let haveVisitedInputs = false;
+  return {
+    [MapperKind.OBJECT_TYPE](type, schema): GraphQLObjectType {
+      if (!haveVisitedInputs) {
+        visitor.addInputTypesValidations(schema, directiveName);
+        haveVisitedInputs = true;
+      }
+      visitor.visitObjectFieldsAndArgumentInputs(type, schema, directiveName);
+      const [directive] = getDirective(schema, type, directiveName) ?? [];
+      if (!directive) return type;
+      // eslint-disable-next-line no-param-reassign
+      visitor.args = directive;
+      visitor.visitObject(type);
+      return type;
+    },
+    [MapperKind.OBJECT_FIELD](
+      fieldConfig,
+      _fieldName,
+      typeName,
+      schema,
+    ): GraphQLFieldConfig<unknown, unknown> {
+      const [directive] =
+        getDirective(schema, fieldConfig, directiveName) ?? [];
+      if (!directive) return fieldConfig;
+      // eslint-disable-next-line no-param-reassign
+      visitor.args = directive;
+      const objectType = schema.getType(typeName);
+      if (isObjectType(objectType)) {
+        visitor.visitFieldDefinition(fieldConfig, {
+          objectType,
         });
-      });
-    });
-    const [directive] = getDirective(schema, type, directiveName) ?? [];
-    if (!directive) return type;
-    // eslint-disable-next-line no-param-reassign
-    visitor.args = directive;
-    visitor.visitObject(type);
-    return type;
-  },
-  [MapperKind.OBJECT_FIELD](
-    fieldConfig,
-    _fieldName,
-    typeName,
-    schema,
-  ): GraphQLFieldConfig<unknown, unknown> {
-    const [directive] = getDirective(schema, fieldConfig, directiveName) ?? [];
-    if (!directive) return fieldConfig;
-    // eslint-disable-next-line no-param-reassign
-    visitor.args = directive;
-    const objectType = schema.getType(typeName);
-    if (isObjectType(objectType)) {
-      visitor.visitFieldDefinition(fieldConfig, {
-        objectType,
-      });
-    }
-    return fieldConfig;
-  },
-});
+      }
+      return fieldConfig;
+    },
+  };
+};
 
 export const createSchemaMapperForVisitor =
   <T extends DirectiveLocation>(
